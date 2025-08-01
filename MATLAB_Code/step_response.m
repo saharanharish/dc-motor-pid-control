62345ad14c1da0eea83364d94a4707193093ed1d
saharanharish <saharanharish85@gmail.com>
2025-08-01 22:55:32 +0530

    Update step_response.m

--- MATLAB_Code/step_response.m
+++ MATLAB_Code/step_response.m
@@ -1,1 +1,25 @@
+Ra = 0.45;                 %Per phase stator resistance
+La = 0.035;              %Per phase stator inductance
+B = 2e-4;              %Damping coefficient
+J = 0.022;             %Moment of Inertia
+Ke = 0.5;                %Torque c0nstant
+Kt = 0.5;                %Back EMF constant
 
+Kp =  4.19652540503482;
+Ki =  35.8889727302617;
+Kd =  0.1197446984208;
+N = 905.389232075555;
+
+a = J*La;
+b = (J*Ra + B*La);
+c = (B*Ra + Ke*Kt);
+
+s=tf('s');
+
+G = tf([0 0 Kt],[a b c])
+
+[w,zeta] = damp(G)  % zeta calculation for transfer function
+C = Kp + Ki/s + ((Kd*s*N)/(s+N)); %PID Controller 
+
+H = feedback(C*G,1);
+step(H) 
